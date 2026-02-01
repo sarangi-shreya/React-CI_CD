fd41640c7f41864faf114a3faaeac1b2492a8924
sarangi-shreya <shreyasarangi2006@gmail.com>
2026-02-01 11:01:06 +0530

    Added codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
         <p>
